27392fffd8855d22f61787568ee66b7aa5bdb832
cliff xuan <cliff.xuan@gmail.com>
2016-04-08 03:35:04 +0000

    functional style

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import React, {Component, createElement} from 'react';
+import React, {Component, createElement, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import superagent from 'superagent';
@@ -85,114 +85,107 @@ class AlbumsContainer extends Component {
 AlbumsContainer.defaultProps = {
   allYears: [1950, 1960, 1970, 1980, 1990, 2000, 2010],
   count: 100
-}
+};
 
 
-class Albums extends Component {
+const Albums = ({albums, years, allYears, onToggleYear, onToggleAlbum}) => (
+  <div id="albums">
+    <YearMenu
+      years={years}
+      allYears={allYears}
+      onToggleYear={year => onToggleYear(year)} />
+    <div className="grids">
+      {albums.map(
+        ([id, album]) =>
+          <Album
+            {...Object.assign(
+              {key: id, onToggle: () => onToggleAlbum(id)}, album)
+            }
+          />
+      )}
+    </div>
+  </div>
+);
 
-  constructor(props) {
-    super(props);
+
+const Album = ({id, name, release_date, uri, artists, images, onToggle, isSelected}) => {
+  var image = images.filter(x => Math.abs(x.width - 300) < 100)[0];
+  return (
+    <section className="flip-item-wrap">
+      <img className="fake-image" src={recordImg} alt="" />
+      <input
+        type="checkbox" className="flipper" id={id}
+        onChange={onToggle}
+        checked={isSelected} hidden />
+      <label htmlFor={id} className="flip-item">
+        <figure className="front"><img src={image.url} alt=""></img></figure>
+        <figure className="back">
+          <img src={image.url} alt=""></img>
+          <div className="flip-item-desc">
+            <h4 className="flip-item-title">{name}</h4>
+            <p>{new Date(Date.parse(release_date)).getFullYear()}</p>
+            <a className="artist-link" href={artists[0].uri}>
+              <p>{artists[0].name}</p>
+            </a>
+            <a className="play-button" href={uri}>
+              <i className="fa fa-play-circle-o fa-4x"></i><
+            /a>
+          </div>
+        </figure>
+      </label>
+    </section>
+  );
+};
+
+
+const YearMenu = ({allYears, years, onToggleYear}) => (
+  <div className="pure-menu pure-menu-horizontal fixed-top">
+    <label style={{ float: 'right', margin: '0 60px 0 0'}}>
+      <img src={logoIcon}/>
+    </label>
+    <label className="pure-menu-heading">Release Year</label>
+    <ul className="pure-menu-list">
+      {allYears.map(year =>
+        <YearButton
+          key={year}
+          year={year}
+          isSelected={years.indexOf(year) != -1}
+          onClick={() => onToggleYear(year)}
+        />)
+      }
+    </ul>
+  </div>
+);
+
+YearMenu.propTypes = {
+  allYears: PropTypes.array.isRequired,
+  years: PropTypes.array.isRequired,
+  onToggleYear: PropTypes.func.isRequired
+};
+
+
+const YearButton = ({year, isSelected, onClick}) => {
+  const labelStyle = {};
+  if (isSelected) {
+    labelStyle.backgroundColor = '#d8d8d8';
   }
-
-  render() {
-    return (
-      <div id="albums">
-        <YearMenu
-          years={this.props.years}
-          allYears={this.props.allYears}
-          onToggleYear={year => this.props.onToggleYear(year)} />
-        <div className="grids">
-          {this.props.albums.map(([id, album]) => createElement(
-            Album, Object.assign({key: id, onToggle: () => this.props.onToggleAlbum(id)}, album)
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
-
-
-class Album extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    var image = this.props.images.filter(x => Math.abs(x.width - 300) < 100)[0];
-    return (
-      <section className="flip-item-wrap">
-        <img className="fake-image" src={recordImg} alt="" />
+  return (
+    <li className="pure-menu-item" onChange={onClick} >
+      <label style={labelStyle} className="pure-menu-link toggle">
         <input
-          type="checkbox" className="flipper" id={this.props.id}
-          onChange={this.props.onToggle}
-          checked={this.props.isSelected} hidden />
-        <label htmlFor={this.props.id} className="flip-item">
-          <figure className="front"><img src={image.url} alt=""></img></figure>
-          <figure className="back">
-            <img src={image.url} alt=""></img>
-            <div className="flip-item-desc">
-              <h4 className="flip-item-title">{this.props.name}</h4>
-              <p>{new Date(Date.parse(this.props.release_date)).getFullYear()}</p>
-              <a className="artist-link" href={this.props.artists[0].uri}><p>{this.props.artists[0].name}</p></a>
-              <a className="play-button" href={this.props.uri}><i className="fa fa-play-circle-o fa-4x"></i></a>
-            </div>
-          </figure>
-        </label>
-      </section>
-    );
-  }
+          name="years" type="checkbox"
+          checked={isSelected} value={year} hidden/>
+        {year}s
+      </label>
+    </li>
+  );
 }
 
-
-class YearMenu extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="pure-menu pure-menu-horizontal fixed-top">
-        <label style={{ float: 'right', margin: '0 60px 0 0'}}><img src={logoIcon}/></label>
-        <label className="pure-menu-heading">Release Year</label>
-        <ul className="pure-menu-list">
-          {this.props.allYears.map(year =>
-            <YearButton
-              key={year}
-              year={year}
-              isSelected={this.props.years.indexOf(year) != -1}
-              onClick={() => this.props.onToggleYear(year)}
-            />)
-          }
-        </ul>
-      </div>
-    );
-  }
-}
-
-
-class YearButton extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const labelStyle = {};
-    if (this.props.isSelected) {
-      labelStyle.backgroundColor = '#d8d8d8';
-    }
-    return (
-      <li className="pure-menu-item" onChange={this.props.onClick} >
-        <label style={labelStyle} className="pure-menu-link toggle">
-          <input name="years" type="checkbox" checked={this.props.isSelected} value={this.props.year} hidden/>
-          {this.props.year}s
-        </label>
-      </li>
-    );
-  }
-}
+YearButton.propTypes = {
+  year: PropTypes.number.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
+};
 
 
 ReactDOM.render(<AlbumsContainer source={dataFile} />,
